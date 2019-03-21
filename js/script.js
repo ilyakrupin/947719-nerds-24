c3b3f818f1722ea23bee76b3d67ca6099f9e991a
Ilya Krupin <dev@krupibox.ru>
2019-03-22 00:02:03 +0300

    Add static map

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ var modal = document.querySelector(".modal");
 var open = document.querySelector(".send-us-popup-button");
 var close = document.querySelector(".modal-close");
 
-open.addEventListener("click", function(evt) {
+open.addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.add("modal-show");
 });
 
-close.addEventListener("click", function(evt) {
+close.addEventListener("click", function (evt) {
     evt.preventDefault();
     modal.classList.remove("modal-show");
 });
